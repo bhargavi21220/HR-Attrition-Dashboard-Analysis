05ce2b09787fe5f9aaf52f992ff0e13ea0936f46
bhargavi21220 <142813951+bhargavi21220@users.noreply.github.com>
2026-06-18 13:45:31 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import base64
+
+def set_background(image_file):
+
+    st.markdown(
+        """
+        <style>
+
+        .stApp {
+            background: linear-gradient(
+                135deg,
+                #000000,
+                #111827,
+                #1f2937
+            );
+        }
+
+        [data-testid="stAppViewContainer"] {
+            background: linear-gradient(
+                135deg,
+                #000000,
+                #111827,
+                #1f2937
+            );
+        }
+
+        .block-container {
+            background: transparent;
+            padding: 2rem;
+        }
+
+        h1, h2, h3, h4, h5, h6 {
+            color: white !important;
+        }
+
+        p, label, span, div {
+            color: white !important;
+        }
+
+        section[data-testid="stSidebar"] {
+            background-color: #0f172a;
+        }
+
+        section[data-testid="stSidebar"] * {
+            color: white !important;
+        }
+
+        [data-testid="metric-container"] {
+            background: rgba(0,0,0,0.55);
+            border: 1px solid rgba(255,255,255,0.2);
+            border-radius: 15px;
+            padding: 20px;
+        }
+
+        [data-testid="metric-container"] * {
+            color: white !important;
+        }
+
+        
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
